0dc7bfb1c9eef1e59a4edded945e5e320cc05f85
Oleg Lobanov <oleg@lobanov.me>
2022-07-22 16:25:02 +0200

    Set typing duration to 1 second

--- service/pidor/pidor_handler.go
+++ service/pidor/pidor_handler.go
@@ -85,7 +85,7 @@ func (s *Service) handlePidor(ctx context.Context, update tgbotapi.Update) error
 		return err
 	}
 
-	return s.sendMessages(update.Message.Chat.ID, messages, 500*time.Millisecond, false)
+	return s.sendMessages(update.Message.Chat.ID, messages, time.Second, false)
 }
 
 func getPidorMessages(user domain.User) ([]Message, error) {
